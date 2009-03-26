54ff2186344c2bb580a5c287190fe4a3045cf13a
emil.webber <gwf@verlagfranz.com>
2009-03-26 23:53:04 +0000

    changed include_once to require_once

--- extension/xrowecommerce/modules/xrowecommerce/multiadd.php
+++ extension/xrowecommerce/modules/xrowecommerce/multiadd.php
@@ -29,7 +29,7 @@
 $http = eZHTTPTool::instance();
 $module =& $Params["Module"];
 
-include_once( "kernel/common/template.php" );
+require_once( "kernel/common/template.php" );
 
 $basket = eZBasket::currentBasket();
 $basket->updatePrices(); // Update the prices. Transaction not necessary.
