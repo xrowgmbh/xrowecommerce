d3a52251f20800e458241d68d12a2d1c3cfd3b53
cel <cel@fa1057a2-fa39-4298-80e7-05e047a5e26b>
2009-04-22 12:32:13 +0000

    * xrowcomdirect ger-DE translation update

--- trunk/extension/xrowcomdirect/translations/ger-DE/translation.ts
+++ trunk/extension/xrowcomdirect/translations/ger-DE/translation.ts
@@ -1,5 +1,103 @@
 <!DOCTYPE TS><TS>
 <context>
+    <name>extension/xrowcomdirect</name>
+    <message>
+        <source>Payment Information</source>
+        <translation>Zahlungsinformationen</translation>
+    </message>
+    <message>
+        <source>Please enter your credit card information</source>
+        <translation>Bitte geben Sie Ihre Kreditkarteninformationen ein</translation>
+    </message>
+    <message>
+        <source>Validation error</source>
+        <translation>Fehler bei der Gültigkeitsüberprüfung</translation>
+    </message>
+    <message>
+        <source>Name on credit card</source>
+        <translation>Name auf der Kreditkarte</translation>
+    </message>
+    <message>
+        <source>Credit card type</source>
+        <translation>Kreditkartentyp</translation>
+    </message>
+    <message>
+        <source>Credit card number</source>
+        <translation>Kreditkartennummer</translation>
+    </message>
+    <message>
+        <source>Credit card security code</source>
+        <translation>Sicherheitsschlüssel der Kreditkarte</translation>
+    </message>
+    <message>
+        <source>Expiration date</source>
+        <translation>Ablaufdatum</translation>
+    </message>
+    <message>
+        <source>Month</source>
+        <translation>Monat</translation>
+    </message>
+    <message>
+        <source>Year</source>
+        <translation>Jahr</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Weiter</translation>
+    </message>
+    <message>
+        <source>What are the advantages of security code verification?</source>
+        <translation>Was sind die Vorteile der Sicherheitsschlüsselüberprüfung?</translation>
+    </message>
+    <message>
+        <source>Please enter the last 3 digits of your credit card&apos;s security code, which is printed on the back of your card.</source>
+        <translation>Bitte geben Sie die letzten 3 Zeichen des Sicherheitsschlüssels auf Ihrer Kreditkarte ein, er ist auf der Rückseite der Karte zu finden.</translation>
+    </message>
+    <message>
+        <source>There are several advantages of security code verification</source>
+        <translation>Es gibt mehrere Vorteile die für die Sicherheitsschlüsselüberprüfung sprechen</translation>
+    </message>
+    <message>
+        <source>Increased customer security</source>
+        <translation>Erhöhte Sicherheit für den Benutzer</translation>
+    </message>
+    <message>
+        <source>Faster order fullfilment</source>
+        <translation>Schnellere Auftragsabwicklung</translation>
+    </message>
+    <message>
+        <source>Deters fraud</source>
+        <translation>Schreckt vom Betrug ab</translation>
+    </message>
+    <message>
+        <source>Please enter your debit card information</source>
+        <translation>Bitte geben Sie Ihre Kreditkarteninformationen ein</translation>
+    </message>
+    <message>
+        <source>Name of account</source>
+        <translation>Name des Kontos</translation>
+    </message>
+    <message>
+        <source>Account number</source>
+        <translation>Kontonummer</translation>
+    </message>
+    <message>
+        <source>Bank code</source>
+        <translation>Bankleitzahl</translation>
+    </message>
+</context>
+<context>
+    <name>extension/xrowecommerce</name>
+    <message>
+        <source>Continue</source>
+        <translation>Weiter</translation>
+    </message>
+</context>
+<context>
     <name>kernel/classes/datatypes</name>
     <message>
         <source>Please enter the name of the bank account.</source>
@@ -36,6 +134,14 @@
     <message>
         <source>The creditcard was not accepted.</source>
         <translation>Die Kreditkarte wurde nicht akzeptiert.</translation>
+    </message>
+    <message>
+        <source>Credit card</source>
+        <translation>Kreditkarte</translation>
+    </message>
+    <message>
+        <source>Direct debit</source>
+        <translation>Abbuchung / Bankeinzug</translation>
     </message>
 </context>
 </TS>
--- trunk/extension/xrowcomdirect/translations/untranslated/translation.ts
+++ trunk/extension/xrowcomdirect/translations/untranslated/translation.ts
@@ -1,5 +1,103 @@
 <!DOCTYPE TS><TS>
 <context>
+    <name>extension/xrowcomdirect</name>
+    <message>
+        <source>Payment Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please enter your credit card information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Validation error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name on credit card</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Credit card type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Credit card number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Credit card security code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Expiration date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Month</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Year</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>What are the advantages of security code verification?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please enter the last 3 digits of your credit card&apos;s security code, which is printed on the back of your card.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>There are several advantages of security code verification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Increased customer security</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Faster order fullfilment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deters fraud</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please enter your debit card information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name of account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Account number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bank code</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>extension/xrowecommerce</name>
+    <message>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>kernel/classes/datatypes</name>
     <message>
         <source>Please enter the name of the bank account.</source>
@@ -35,6 +133,14 @@
     </message>
     <message>
         <source>The creditcard was not accepted.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Credit card</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Direct debit</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
