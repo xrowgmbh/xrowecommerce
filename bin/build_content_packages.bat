83191806bb86aaa6833402ef194063747069b31d
xrow <bjoern@xrow.de>
2010-02-26 08:16:32 +0000

    altered some paths

--- bin/build_content_packages.bat
+++ bin/build_content_packages.bat
@@ -14,14 +14,14 @@ RMDIR /S /Q var\storage\packages\local
 "C:\Program Files (x86)\Zend\Zend Studio - 7.0.1\plugins\org.zend.php.debug.debugger.win32.x86_5.2.26.v20090817\resources\php5\php-win.exe" ezpm.php -s ezwebin_site_admin add xrowecommerce_content ezcontentobject --exclude-classes --exclude-templates --current-version "democontent/*" "manufacturer/*" "shop2" "news/*" "beer/*" "coupons/*" "products/*"
 RMDIR /S /Q var\storage\packages\local\xrowecommerce_classes\.cache
 RMDIR /S /Q var\storage\packages\local\xrowecommerce_content\.cache
-cd C:\svn\xrowecommerce\unstable\buildtest\packages
+cd C:\svn\xrowecommerce\trunk\packages
 svn rm xrowecommerce_content
 svn rm xrowecommerce_classes
 RMDIR /S /Q xrowecommerce_classes
 RMDIR /S /Q xrowecommerce_content
 svn commit -m"remove packages"
-XCOPY /Y /E /I C:\workspace\shop\var\storage\packages\local\xrowecommerce_classes C:\svn\xrowecommerce\unstable\buildtest\packages\xrowecommerce_classes
-XCOPY /Y /E /I C:\workspace\shop\var\storage\packages\local\xrowecommerce_content C:\svn\xrowecommerce\unstable\buildtest\packages\xrowecommerce_content
+XCOPY /Y /E /I C:\workspace\shop\var\storage\packages\local\xrowecommerce_classes C:\svn\xrowecommerce\trunk\packages\xrowecommerce_classes
+XCOPY /Y /E /I C:\workspace\shop\var\storage\packages\local\xrowecommerce_content C:\svn\xrowecommerce\trunk\packages\xrowecommerce_content
 svn add xrowecommerce_content
 svn add xrowecommerce_classes
 svn commit -m"add packages"
