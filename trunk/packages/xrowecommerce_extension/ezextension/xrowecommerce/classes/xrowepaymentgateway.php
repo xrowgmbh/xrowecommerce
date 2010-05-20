1fe5e0c5dc67c00f383336ffd9d23dea6b13f7c1
xrow <xrow@fa1057a2-fa39-4298-80e7-05e047a5e26b>
2010-05-20 13:48:46 +0000

    optimize the encode/decode functions

--- trunk/packages/xrowecommerce_extension/ezextension/xrowecommerce/classes/xrowepaymentgateway.php
+++ trunk/packages/xrowecommerce_extension/ezextension/xrowecommerce/classes/xrowepaymentgateway.php
@@ -37,28 +37,52 @@ class xrowEPaymentGateway extends eZPaymentGateway
         return true;
     }
 
-    static function encryptData( $planeTextData, $key, $algorithm, $mode )
+    static function encryptData( $planeTextData )
     {
-        foreach ( $planeTextData as $planeTextItem )
+    	$xrowini = eZINI::instance( 'xrowecommerce.ini' );
+        $key = $xrowini->variable( 'EncryptionSettings', 'Key' );
+        $algorithm = $xrowini->variable( 'EncryptionSettings', 'Algorithm' );
+        $mode = $xrowini->variable( 'EncryptionSettings', 'Mode' );
+        if ( is_object( $planeTextData ) || is_array( $planeTextData ) )
         {
-            $planeText = $planeTextItem;
+	        foreach ( $planeTextData as $planeTextItem )
+	        {
+	            $planeText = $planeTextItem;
+	        }
         }
-        $td = mcrypt_module_open( 'tripledes', '', 'cfb', '' );
+        else
+        {
+        	$planeText = $planeTextData;
+        }
+        $td = mcrypt_module_open( $algorithm, '', $mode, '' );
         $iv = mcrypt_create_iv( mcrypt_enc_get_iv_size( $td ), MCRYPT_RAND );
         $okey = substr( md5( $key . rand( 0, 9 ) ), 0, mcrypt_enc_get_key_size( $td ) );
         mcrypt_generic_init( $td, $okey, $iv );
         $encrypted = mcrypt_generic( $td, $planeText . chr( 194 ) );
         $encryptedString = $encrypted . $iv;
-        $encryptedString = eregi_replace( "'", "'", $encryptedString );
+        
         return base64_encode( $encryptedString );
     }
 
-    static function decryptData( $encryptedStringData, $key, $algorithm, $mode )
+    static function decryptData( $encryptedStringData, $limit = 0 )
     {
-        foreach ( $encryptedStringData as $encryptedStringDataItem )
+    	$xrowini = eZINI::instance( 'xrowecommerce.ini' );
+        $key = $xrowini->variable( 'EncryptionSettings', 'Key' );
+        $algorithm = $xrowini->variable( 'EncryptionSettings', 'Algorithm' );
+        $mode = $xrowini->variable( 'EncryptionSettings', 'Mode' );
+        
+        if ( is_object( $encryptedStringData ) || is_array( $encryptedStringData ) )
         {
-            $encryptedString = base64_decode( $encryptedStringDataItem );
+	        foreach ( $encryptedStringData as $encryptedStringDataItem )
+	        {
+	            $encryptedString = base64_decode( $encryptedStringDataItem );
+	        }
         }
+        else
+        {
+        	$encryptedString = base64_decode( $encryptedStringData );
+        }
+        
         $td = mcrypt_module_open( $algorithm, '', $mode, '' );
         $iv = substr( $encryptedString, - 8 );
         $encrypted = substr( $encryptedString, 0, - 8 );
@@ -69,10 +93,16 @@ class xrowEPaymentGateway extends eZPaymentGateway
             $decrypted = trim( mdecrypt_generic( $td, $encrypted ) );
             mcrypt_generic_deinit( $td );
             $planeText = substr( $decrypted, 0, - 1 );
-            if ( ord( substr( $decrypted, - 1 ) ) == 194 && is_numeric( $planeText ) )
+            if ( ord( substr( $decrypted, - 1 ) ) == 194  )
                 break;
         }
         mcrypt_module_close( $td );
+        // cut the result, if a limit is greater than 0
+    	if ( $limit > 0 )
+        {
+            $planeText = strrev( substr( strrev( $planeText ), 0, 4 ) );
+        } 
+        
         return $planeText;
     }
 }
