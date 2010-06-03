89dd5600fc341a99f47cc61832ed194b748445cc
cel <cel@fa1057a2-fa39-4298-80e7-05e047a5e26b>
2010-06-03 13:23:47 +0000

    multipricetemplateoperator changes: error message, replaced tabs

--- trunk/packages/xrowecommerce_extension/ezextension/xrowecommerce/autoloads/getmultipricetemplateoperator.php
+++ trunk/packages/xrowecommerce_extension/ezextension/xrowecommerce/autoloads/getmultipricetemplateoperator.php
@@ -51,60 +51,66 @@ class getMutliPriceTemplateOperator
     	$price = $namedParameters['price'];
         $optionPriceObj = $namedParameters['option_price'];
         $priceparameter = $namedParameters['priceparameter'];
-        
+
         $parentPriceObj = false;
         $vat_percent = 0;
         $discount_percent = 0;
         $price_parent = 0.00;
         $price_option = 0.00;
-        
+
         if (is_object( $price ))
         {
-	        $dataType = $price->dataType();                       
-			if ( $dataType->isA() == 'ezmultiprice' )
-	        {
-				$parentPriceObj = $price->content();
-            	// get the vat value
-            	$vat_percent = $parentPriceObj->VATType->getPercentage( false, '' );
-            	// get the discount value
-            	$discount_percent = $parentPriceObj->DiscountPercent;
-            	// get the option price
-            	$price_option = $optionPriceObj->attribute( 'price' );
-		        switch ( $operatorName )
-		        {
-		            case 'get_multiprice':
-		                
-		            	$price_parent = $parentPriceObj->attribute( $priceparameter );
+            $dataType = $price->dataType();
+            if ( $dataType->isA() == 'ezmultiprice' )
+            {
+                $parentPriceObj = $price->content();
+                // get the vat value
+                $vat_percent = $parentPriceObj->VATType->getPercentage( false, '' );
+                // get the discount value
+                $discount_percent = $parentPriceObj->DiscountPercent;
+                // get the option price
+                $price_option = $optionPriceObj->attribute( 'price' );
+               
+                switch ( $operatorName )
+                {
+                    case 'get_multiprice':
+                        
+                        $price_parent = $parentPriceObj->attribute( $priceparameter );
 
-		            	switch ( $priceparameter )
-		            	{
-		            		case 'inc_vat_price':
-		            		case 'discount_price_inc_vat':
-		            			if ( $vat_percent > 0 && !$parentPriceObj->IsVATIncluded )
-						        {
-						        	$price_option = $price_option * ( 100 + $vat_percent ) / 100;
-						        }
-						        
-		            		break;
-		            		
-		            		case 'ex_vat_price':
-		            		case 'discount_price_ex_vat':
-		            			if ( $vat_percent > 0 && $parentPriceObj->IsVATIncluded )
-						        {
-						        	$price_option = $price_option / ( 100 + $vat_percent ) / 100;
-						        }
-		            		break;
-		            	}
-		        		if ( $price_option > 0 && $discount_percent > 0 && ( $priceparameter == 'discount_price_inc_vat' || $priceparameter == 'discount_price_ex_vat' ) )
-		        		{
-							$price_option = $price_option * ( 100 - $discount_percent ) / 100;
-						}
-		            	$total_price = $price_parent + $price_option;
-		            	
-						$operatorValue = $total_price;
-		            break;
-		        }
-	        }
+                        switch ( $priceparameter )
+                        {
+                            case 'inc_vat_price':
+                            case 'discount_price_inc_vat':
+
+                                if ( $vat_percent > 0 && !$parentPriceObj->IsVATIncluded )
+                                {
+                                    $price_option = $price_option * ( 100 + $vat_percent ) / 100;
+                                }
+                                
+                            break;
+
+                            case 'ex_vat_price':
+                            case 'discount_price_ex_vat':
+                                if ( $vat_percent > 0 && $parentPriceObj->IsVATIncluded )
+                                {
+                                    $price_option = $price_option / ( 100 + $vat_percent ) / 100;
+                                }
+                            break;
+                        }
+                        if ( $price_option > 0 && $discount_percent > 0 && ( $priceparameter == 'discount_price_inc_vat' || $priceparameter == 'discount_price_ex_vat' ) )
+                        {
+                            $price_option = $price_option * ( 100 - $discount_percent ) / 100;
+                        }
+                        $total_price = $price_parent + $price_option;
+                        
+                        $operatorValue = $total_price;
+                    break;
+                }
+            }
+            else
+            {
+            	eZDebug::writeError( 'Datatype is not a multiprice', __METHOD__ );
+            }
         }
     }
 }
