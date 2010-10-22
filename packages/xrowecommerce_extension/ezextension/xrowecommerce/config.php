354c0b9b10d268b4a442d595178e96710759c1df
emil.webber <gwf@verlagfranz.com>
2010-10-22 11:57:10 +0000

    Added readme and str_getcsv alternative to extension

---
+++ packages/xrowecommerce_extension/ezextension/xrowecommerce/config.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!function_exists('str_getcsv'))
+{
+    function str_getcsv($input, $delimiter = ",", $enclosure = '"', $escape = "\\")
+    {
+        $fiveMBs = 5 * 1024 * 1024;
+        $fp = fopen("php://temp/maxmemory:$fiveMBs", 'r+');
+        fputs($fp, $input);
+        rewind($fp);
+
+        $data = fgetcsv($fp, 1000, $delimiter, $enclosure); //  $escape only got added in 5.3.0
+
+        fclose($fp);
+        return $data;
+    }
+}
+
+?>
