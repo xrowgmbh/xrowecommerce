dda498e68d51dbbef90e5ca10ddfcef95f04cb3a
cel <christian@xrow.de>
2011-01-26 14:11:36 +0000

    Added "Free Shipping" and "Free Handling" Feature.

--- packages/xrowecommerce_extension/ezextension/xrowecommerce/settings/shipping.ini.append.php
+++ packages/xrowecommerce_extension/ezextension/xrowecommerce/settings/shipping.ini.append.php
@@ -3,8 +3,27 @@ ShippingGateways[]=UPS
 ShippingGateways[]=USPS
 ShippingGateways[]=fixedprice
 
-# List of Products that do not have shipping costs
+# List of products that do not have shipping and/or handling costs (contentobject ID)
 FreeShippingProducts[]
+##example:
+##FreeShippingProducts[]=1234
+
+# Allows free shipping and/or handling even with additional non free shipping products
+FreeShippingAdditionalProducts=disabled
+
+# Amount of products when shipping and/or handling is free
+FreeShippingProductConditions[]
+##example: Shipping is free, when 2 or more items from contentobject 1234 are in the cart
+##FreeShippingProductConditions[1234]=2
+ 
+# List of gateways where free shipping and/or handling is allowed (gateway identifiers)
+FreeShippingHandlingGateways[]
+
+# List of countries where free shipping and/or handling is allowed (Alpha3)
+FreeShippingHandlingCountries[]
+##example:
+##FreeShippingHandlingCountries[GER]
+
 # Switch to enable or disable Handling
 HandlingFee=enabled
 # Amount added to the cart
