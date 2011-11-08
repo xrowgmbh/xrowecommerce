ddbde63f6e2d5d27c00756a54d46eb2a2defdac0
cel <christian@xrow.de>
2011-11-08 15:14:47 +0000

    reworked ezpaypal extension

--- extension/ezpaypal/ezinfo.php
+++ extension/ezpaypal/ezinfo.php
@@ -4,8 +4,8 @@ class ezpaypalInfo
     function info()
     {
         return array( 'Name' => "eZ Paypal Payment Gateway",
-                      'Version' => '1.0',
-                      'Copyright' => "Copyright (C) 1999-2006 eZ Systems AS",
+                      'Version' => "1.0",
+                      'Copyright' => "Copyright (C) 1999-2010 eZ systems AS",
                       'License' => "GNU General Public License v2.0"
                      );
     }
