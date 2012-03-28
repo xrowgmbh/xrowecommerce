703dd54ffbbb598b42fdc0cd257bb2d2b96cb2b5
cel <cel@fa1057a2-fa39-4298-80e7-05e047a5e26b>
2012-03-28 08:15:50 +0000

    added workflowprocess_id to sql schema

--- trunk/packages/xrowecommerce_extension/ezextension/xrowecommerce/sql/mysql/mysql.sql
+++ trunk/packages/xrowecommerce_extension/ezextension/xrowecommerce/sql/mysql/mysql.sql
@@ -1,14 +1,15 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `xrowpaymentobject`;
-CREATE TABLE xrowpaymentobject (
-  id int(11) NOT NULL auto_increment,
-  order_id int(11) NOT NULL default '0',
-  payment_string varchar(255) NOT NULL default '',
-  status int(11) NOT NULL default '0',
-  data text NOT NULL,
-  PRIMARY KEY  (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE  `xrowpaymentobject` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL DEFAULT '0',
+  `payment_string` varchar(255) NOT NULL DEFAULT '',
+  `status` int(11) NOT NULL DEFAULT '0',
+  `data` text NOT NULL,
+  `workflowprocess_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
 
 INSERT INTO ezorder_status (id, is_active, name, status_id) VALUES ( null,1,'Canceled',1000);
 
