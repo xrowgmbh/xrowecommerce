98a3c7dfb11b40b3e5a9380c027ff80199458a7a
cel <christian@xrow.de>
2012-06-01 06:50:36 +0000

    Roles and policies bugfix: added custumersearch/multiple to FunctionList array

--- packages/xrowecommerce_extension/ezextension/xrowecommerce/modules/customersearch/module.php
+++ packages/xrowecommerce_extension/ezextension/xrowecommerce/modules/customersearch/module.php
@@ -1,14 +1,26 @@
 <?php
-$Module = array( "name" => "Customersearch" );
+$Module = array( 
+    "name" => "Customersearch" 
+);
 
 $ViewList = array();
-$ViewList["search"] = array(
-    "functions" => array( "customersearch" ),
-    "script" => "search.php");
-$ViewList["multiple"] = array(
-    "functions" => array( "multiple" ),
-    "script" => "multiple.php",
-    "params" => array( "email" ) );
+$ViewList["search"] = array( 
+    "functions" => array( 
+        "customersearch" 
+    ) , 
+    "script" => "search.php" 
+);
+$ViewList["multiple"] = array( 
+    "functions" => array( 
+        "multiple" 
+    ) , 
+    "script" => "multiple.php" , 
+    "params" => array( 
+        "email" 
+    ) 
+);
 
-$FunctionList["customersearch"] = array( );
+$FunctionList["customersearch"] = array();
+$FunctionList["multiple"] = array();
+
 ?>
