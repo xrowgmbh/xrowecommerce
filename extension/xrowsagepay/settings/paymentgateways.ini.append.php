4048a90441554fc3585b9301dc4a624292ab7f32
Cel <Cel@fa1057a2-fa39-4298-80e7-05e047a5e26b>
2012-10-19 13:18:50 +0000

    + sagepay added (unfinished)

---
+++ extension/xrowsagepay/settings/paymentgateways.ini.append.php
@@ -0,0 +1,12 @@
+<?php /* #?ini charset="utf-8"?
+
+[GatewaysSettings]
+AvailableGateways[]=xrowInvoice
+AvailableGateways[]=xrowCashOnDelivery
+AvailableGateways[]=xrowAdvancepayment
+AvailableGateways[]=xrowSagePay
+
+#[CashOnDeliverySettings]
+#Costs=0
+
+*/ ?>
