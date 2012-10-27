9195491b6790af5fa7203552db8b3108b0e97ddd
xrow <bjoern@xrow.de>
2012-10-27 16:26:48 +0000

    - Some fixes

--- extension/xrowsagepay/settings/paymentgateways.ini.append.php
+++ extension/xrowsagepay/settings/paymentgateways.ini.append.php
@@ -1,9 +1,6 @@
 <?php /* #?ini charset="utf-8"?
 
 [GatewaysSettings]
-AvailableGateways[]=xrowInvoice
-AvailableGateways[]=xrowCashOnDelivery
-AvailableGateways[]=xrowAdvancepayment
 AvailableGateways[]=xrowSagePay
 
 #[CashOnDeliverySettings]
